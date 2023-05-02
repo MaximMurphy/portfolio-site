a29f1f036eb5dd0042ad90267a8d0b9a8b82e193
Maxim Murphy <maxim.murphy8@gmail.com>
2023-05-01 21:13:18 -0400

    created rotating animation for logo

--- src/components/LegoModel.jsx
+++ src/components/LegoModel.jsx
@@ -12,8 +12,8 @@ THREE.DefaultLoadingManager.addHandler(/\.dds$/i, new DDSLoader());
 
 const Scene = (props) => {
   const ref = useRef();
-  const materials = useLoader(MTLLoader, "3DModel/Maxim.mtl");
-  const obj = useLoader(OBJLoader, "3DModel/Maxim.obj", (loader) => {
+  const materials = useLoader(MTLLoader, "3DModel2/Maxim.mtl");
+  const obj = useLoader(OBJLoader, "3DModel2/Maxim.obj", (loader) => {
     materials.preload();
     loader.setMaterials(materials);
   });
@@ -21,9 +21,9 @@ const Scene = (props) => {
   useFrame((state) => {
     const t = state.clock.getElapsedTime();
     ref.current.rotation.set(
-      Math.cos(t / 4) / 8,
-      Math.sin(t / 3) / 4,
-      0.15 + Math.sin(t / 2) / 8
+      Math.cos(t / 2) / 8,
+      Math.sin(t / 1.5) / 4,
+      0.15 + Math.sin(t / 0.8) / 8
     );
     ref.current.position.y = (0.5 + Math.cos(t / 2)) / 7;
   });
@@ -31,26 +31,27 @@ const Scene = (props) => {
   console.log(obj);
   return (
     <group ref={ref}>
-      <primitive object={obj} scale={0.055} />
+      <primitive object={obj} scale={0.08} position={(0, 0, -2)} />
     </group>
   );
 };
 
 export default function LegoModel() {
   return (
-    <div className="h-full">
+    <div className="h-36 w-screen lg:w-96">
       <Canvas
+        className="h-full"
         shadows
         camera={{
-          position: [10, 1, 0],
+          position: [10, 1, 1.2],
         }}
       >
-        <ambientLight color={"white"} intensity={0.2} />
+        <ambientLight color={"white"} intensity={0.8} />
         <pointLight castShadow />
 
         <Suspense fallback={null}>
           <Scene />
-          <OrbitControls />
+          {/*<OrbitControls />*/}
         </Suspense>
       </Canvas>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import LegoModel from "@/components/LegoModel";
 
 export default function Navbar() {
   return (
-    <nav className="sticky w-full top-0 z-50 flex flex-col items-center justify-between px-24 pt-24 pb-4 gap-y-12 text-base tracking-widest lg:flex-row lg:px-24">
+    <nav className="sticky w-full top-0 z-50 flex flex-col items-center justify-between pt-4 lg:pt-8 pb-4 gap-y-6 text-base tracking-widest lg:flex-row lg:px-24">
       <Link href="/">
         <LegoModel />
       </Link>
