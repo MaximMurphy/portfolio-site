02e7e91b1b9ce502ed81dce9416ad73229dd35ec
Maxim Murphy <maxim.murphy8@gmail.com>
2023-05-05 11:52:48 -0400

    added scolling to view overflow for about cards

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -8,7 +8,7 @@ export default function about() {
           About Me
         </h1>
         <section className="flex flex-col lg:flex-row justify-between gap-12">
-          <div className="w-fit h-full bg-white/20 rounded-xl backdrop-blur border-solid border border-white/20 shadow-xl flex flex-col items-center text-center justify-center p-8 gap-4">
+          <div className="w-fit h-[500px] overflow-y-scroll bg-white/20 rounded-xl backdrop-blur border-solid border border-white/20 shadow-xl flex flex-col items-center text-center p-8 gap-4">
             <Image
               src="/about/osu.png"
               alt="OSU Logo"
@@ -22,7 +22,7 @@ export default function about() {
               specialization was in Software Engineering.
             </p>
           </div>
-          <div className="w-fit h-full bg-white/20 rounded-xl backdrop-blur border-solid border border-white/20 shadow-xl flex flex-col items-center text-center justify-center p-8 gap-4">
+          <div className="w-fit h-[500px] overflow-y-scroll bg-white/20 rounded-xl backdrop-blur border-solid border border-white/20 shadow-xl flex flex-col items-center text-center p-8 gap-4">
             <Image
               src="/about/chm.png"
               alt="Plast Chornomortsi Logo"
@@ -31,11 +31,16 @@ export default function about() {
               className="w-36 h-36"
             />
             <p>
-              I am actively invovlved in the Ukrainain Scouting Organization,
-              Plast.
+              I am actively invovlved in the Ukrainian Scouting Organization,
+              Plast. I have been a summer camp counselor since 2017 and in 2021,
+              I joined a subdivision known as the Chornomortsi. Our focus is
+              boating and planning a water sports camp. In 2022, I was the
+              administrator for this camp with the resposibility of creating
+              registration forms, accepting applicants, and being in charge of
+              the bank account.
             </p>
           </div>
-          <div className="w-fit h-full bg-white/20 rounded-xl backdrop-blur border-solid border border-white/20 shadow-xl flex flex-col items-center text-center justify-center p-8 gap-4">
+          <div className="w-fit h-[500px] overflow-y-scroll bg-white/20 rounded-xl backdrop-blur border-solid border border-white/20 shadow-xl flex flex-col items-center text-center p-8 gap-4">
             <Image
               src="/about/musicNotes.png"
               alt="Music Note"
