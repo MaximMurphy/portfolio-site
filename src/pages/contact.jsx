53f6cecbc7e53215bae50d5f7c7c2ffb944d78f5
Maxim Murphy <maxim.murphy8@gmail.com>
2023-05-05 15:33:25 -0400

    form submission sends data in post request

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,4 +1,24 @@
-export default function Contact() {
+import React from "react";
+
+export default function contact() {
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const data = new FormData(e.currentTarget);
+    try {
+      const response = await fetch("/api/contact", {
+        method: "post",
+        body: new URLSearchParams(data),
+      });
+      if (!response.ok) {
+        throw new Error(`Invalid response: ${response.status}`);
+      }
+      alert("Thanks for contacting us, we will get back to you soon!");
+    } catch (err) {
+      console.error(err);
+      alert("There was an error submitting the form :(");
+    }
+  }
+
   return (
     <section className="bg-gradient-to-b from-softblue-400 via-softblue-600 to-softblue-800 w-full flex flex-col px-12 lg:px-48 mt-12 lg:mt-0 gap-12 lg:gap-24 pb-24">
       <h1 className="text-7xl lg:text-9xl font-bold text-end">Contact</h1>
@@ -9,12 +29,16 @@ export default function Contact() {
         maxim.murphy8@gmail.com
       </p>
 
-      <form className="z-40 w-full h-full bg-white/10 rounded-xl backdrop-blur-sm border-solid border border-white/10 shadow-xl flex flex-col items-start justify-center text-white font-bold p-12 lg:p-16">
+      <form
+        onSubmit={handleSubmit}
+        className="z-40 w-full h-full bg-white/10 rounded-xl backdrop-blur-sm border-solid border border-white/10 shadow-xl flex flex-col items-start justify-center text-white font-bold p-12 lg:p-16"
+      >
         <label htmlFor="name" className="mb-4">
           {" "}
           Your Name
         </label>
         <input
+          id="name"
           type="text"
           autoComplete="family-name"
           required
@@ -25,16 +49,18 @@ export default function Contact() {
           Email
         </label>
         <input
+          id="email"
           type="email"
           autoComplete="email"
           required
           name="email"
           className="w-full h-12 rounded-md focus:outline-none focus:ring-2 ring-orange-400 text-blue-950 p-4 mb-12"
         />
-        <label htmlFor="email" className="mb-4">
+        <label htmlFor="message" className="mb-4">
           Message
         </label>
         <textarea
+          id="message"
           type="text"
           rows="6"
           className="w-full h-36 flex items-start rounded-md focus:outline-none focus:ring-2  ring-orange-400 text-blue-950 p-4 mb-12"
