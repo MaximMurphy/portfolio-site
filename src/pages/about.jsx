5f45e8d6395def2ba664872d3652f4ae528f70a7
Maxim Murphy <maxim.murphy8@gmail.com>
2023-05-08 15:59:54 -0400

    finalize about page card content

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 export default function about() {
   return (
     <section className="bg-blue-950 w-full flex flex-col text-white text-lg lg:text-2xl font-bold tracking-wider px-12 lg:px-32 pt-12 gap-12 -mt-96">
-      <h1 className="text-5xl lg:text-9xl font-bold text-center lg:text-end mt-96 pb-8">
+      <h1 className="text-5xl lg:text-9xl font-bold text-center lg:text-end mt-96 lg:pb-8">
         About Me
       </h1>
       <section className="flex flex-col items-center lg:flex-row justify-between gap-12">
-        <div className="z-10 w-fit h-[550px] overflow-y-scroll bg-blue-950 rounded-3xl shadow-neueInset flex flex-col items-center text-center p-8 gap-4">
+        <div className="z-10 w-fit h-[550px] overflow-y-scroll bg-blue-950 rounded-3xl shadow-neueInset flex flex-col items-center text-center lg:text-start p-8 gap-8">
           <Image
             src="/about/osu.png"
             alt="OSU Logo"
@@ -16,14 +16,14 @@ export default function about() {
             className="w-36 h-36"
           />
           <p>
-            I graduated from The Ohio State University with a degree in Computer
-            Science and Engineering in December 2022. My specialization was in
-            Software Engineering. Some of my favorites classes were: Capstone
-            Design - Software Applications, Mobile Application Development, Web
-            Applications, and Computer Game & Animation Techniques
+            I graduated from The Ohio State University in December 2022 with a
+            degree in Computer Science and Engineering, specializing in Software
+            Engineering. Key courses included Capstone Design - Software
+            Applications, Mobile Application Development, Web Applications, and
+            Computer Game & Animation Techniques.
           </p>
         </div>
-        <div className="z-10 w-fit h-[550px] overflow-y-scroll bg-blue-950 rounded-3xl shadow-neueInset flex flex-col items-center text-center p-8 gap-4">
+        <div className="z-10 w-fit h-[550px] overflow-y-scroll bg-blue-950 rounded-3xl shadow-neueInset flex flex-col items-center text-center lg:text-start p-8 gap-8">
           <Image
             src="/about/chm.png"
             alt="Plast Chornomortsi Logo"
@@ -32,16 +32,16 @@ export default function about() {
             className="w-36 h-36"
           />
           <p>
-            I am actively invovlved in the Ukrainian Scouting Organization,
-            Plast. I have been a summer camp counselor since 2017 and in 2021, I
-            joined a subdivision known as the Chornomortsi. Our focus is boating
-            and planning a water sports camp. Since 2022, I&apos;ve been the
-            administrator for this camp with the resposibility of creating
-            registration forms, accepting applicants, and being in charge of the
-            bank account.
+            Actively involved in the Ukrainian Scouting Organization, Plast, my
+            entire life. I have been a summer camp counselor since 2017. In
+            2021, I joined the Chornomortsi subdivision with a focus on boating
+            and running a water sports camp. Currently, I serve as the camp
+            administrator, responsible for developing registration forms,
+            coordinating logistics for gear transportation, and overseeing
+            financial operations.
           </p>
         </div>
-        <div className="z-10 w-fit h-[550px] overflow-y-scroll bg-blue-950 rounded-3xl shadow-neueInset flex flex-col items-center text-center p-8 gap-4">
+        <div className="z-10 w-fit h-[550px] overflow-y-scroll bg-blue-950 rounded-3xl shadow-neueInset flex flex-col items-center text-center lg:text-start p-8 gap-8">
           <Image
             src="/about/musicNotes.png"
             alt="Music Note"
@@ -50,8 +50,8 @@ export default function about() {
             className="w-36 h-32 invert"
           />
           <p>
-            Outside of web development, my main interests are making electronic
-            music, cooking, 35mm film photography, and hiking.
+            Beyond web development, I have a passion for electronic music
+            production, culinary arts, 35mm film photography, and travel.
           </p>
         </div>
       </section>
