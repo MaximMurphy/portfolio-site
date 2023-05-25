076dd842e481815d56a7f223edb4a61ad70f92a4
Maxim Murphy <maxim.murphy8@gmail.com>
2023-05-24 21:30:33 -0400

    Redo Skills section layout

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,52 +6,52 @@ export default function Skills() {
       <h1 className="text-cyan-950 text-lg flex justify-start mb-2 font-semibold underline">
         SKILLS
       </h1>
-      <div className="grid grid-cols-2 lg:grid-cols-4 justify-items-center bg-gradient-to-b from-blue-950/90 to-cyan-950/90 rounded-xl border-2 border-blue-950 text-offwhite-400 text-base lg:text-xl tracking-wider py-8 lg:py-10 px-4 lg:px-2 gap-10 lg:gap-12">
-        <div className="flex flex-row gap-1 lg:gap-2">
+      <div className="grid grid-cols-2 lg:grid-cols-4 text-center justify-items-center text-offwhite-400 text-base lg:text-xl tracking-wider py-8 lg:py-10 px-4 lg:px-2 gap-10 lg:gap-12">
+        <div className="flex flex-row gap-1 lg:gap-2 w-36 lg:w-48 justify-center bg-blue-950 p-8">
           <p>ReactJS</p>
           <Icon className="mt-1" icon="simple-icons:react" />
         </div>
-        <div className="flex flex-row gap-1 lg:gap-2">
+        <div className="flex flex-row gap-1 lg:gap-2 w-36 lg:w-48 justify-center bg-cyan-950 p-8">
           <p>NextJS</p>
           <Icon className="mt-1" icon="simple-icons:nextdotjs" />
         </div>
-        <div className="flex flex-row gap-1 lg:gap-2">
+        <div className="flex flex-row gap-1 lg:gap-2 w-36 lg:w-48 justify-center bg-cyan-950 lg:bg-blue-950 p-8">
           <p>Typescript</p>
           <Icon className="mt-1" icon="simple-icons:typescript" />
         </div>
-        <div className="flex flex-row gap-1 lg:gap-2">
+        <div className="flex flex-row gap-1 lg:gap-2 w-36 lg:w-48 justify-center bg-blue-950 lg:bg-cyan-950 p-8">
           <p>Tailwind</p>
           <Icon className="mt-1" icon="simple-icons:tailwindcss" />
         </div>
-        <div className="flex flex-row gap-1 lg:gap-2">
+        <div className="flex flex-row gap-1 lg:gap-2 w-36 lg:w-48 justify-center bg-blue-950 lg:bg-cyan-950 p-8">
           <p>HTML</p>
           <Icon className="mt-1" icon="simple-icons:html5" />
         </div>
-        <div className="flex flex-row gap-1 lg:gap-2">
+        <div className="flex flex-row gap-1 lg:gap-2 w-36 lg:w-48 justify-center bg-cyan-950 lg:bg-blue-950 p-8">
           <p>CSS</p>
           <Icon className="mt-1" icon="simple-icons:css3" />
         </div>
-        <div className="flex flex-row gap-1 lg:gap-2">
+        <div className="flex flex-row gap-1 lg:gap-2 w-36 lg:w-48 justify-center bg-cyan-950 p-8">
           <p>Javascript</p>
           <Icon className="mt-1" icon="simple-icons:javascript" />
         </div>
-        <div className="flex flex-row gap-1 lg:gap-2">
+        <div className="flex flex-row gap-1 lg:gap-2 w-36 lg:w-48 justify-center bg-blue-950 p-8">
           <p>NodeJS</p>
           <Icon className="mt-1" icon="simple-icons:nodedotjs" />
         </div>
-        <div className="flex flex-row gap-1 lg:gap-2">
+        <div className="flex flex-row gap-1 lg:gap-2 w-36 lg:w-48 justify-center bg-blue-950 p-8">
           <p>Java</p>
           <Icon className="mt-1" icon="mdi:language-java" />
         </div>
-        <div className="flex flex-row gap-1 lg:gap-2">
+        <div className="flex flex-row gap-1 lg:gap-2 w-36 lg:w-48 justify-center bg-cyan-950 p-8">
           <p>Firebase</p>
           <Icon className="mt-1" icon="simple-icons:firebase" />
         </div>
-        <div className="flex flex-row gap-1 lg:gap-2">
+        <div className="flex flex-row gap-1 lg:gap-2 w-36 lg:w-48 justify-center bg-cyan-950 lg:bg-blue-950 p-8">
           <p>Git</p>
           <Icon className="mt-1" icon="simple-icons:git" />
         </div>
-        <div className="flex flex-row gap-1 lg:gap-2">
+        <div className="flex flex-row gap-1 lg:gap-2 w-36 lg:w-48 justify-center bg-blue-950 lg:bg-cyan-950 p-8">
           <p>Jest</p>
           <Icon className="mt-1" icon="simple-icons:jest" />
         </div>
--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 export default function Socials() {
   return (
-    <section className="flex flex-col px-10 lg:px-24 gap-12 font-base tracking-wider">
+    <section className="flex flex-col px-10 lg:px-24 gap-24 font-base tracking-wider pb-4">
       <div>
-        <p className="w-full text-cyan-700 text-2xl text-justify leading-8 lg:leading-9">
+        <p className="w-full text-cyan-800 text-2xl lg:text-3xl text-justify leading-8 lg:leading-10">
           Maxim Murphy is a software developer who holds a degree in Computer
           Science & Engineering from The Ohio State University. With a strong
           focus on full stack web development and design, he combines technical
